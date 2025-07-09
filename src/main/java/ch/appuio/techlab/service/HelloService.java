7869bb42eb7b3835c5a868da1dfff7d75ca32ca4
lizhigang <lizhigang@magictavern.com>
2025-07-09 16:13:11 +0800

    * delete the record after 6s in deleteHelloAsync

--- src/main/java/ch/appuio/techlab/service/HelloService.java
+++ src/main/java/ch/appuio/techlab/service/HelloService.java
@@ -1,5 +1,7 @@
 package ch.appuio.techlab.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -8,11 +10,20 @@ import ch.appuio.techlab.repository.HelloRepository;
 
 @Service
 public class HelloService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloService.class);
     @Autowired
     private HelloRepository helloRepository;
 
     @Async
     public void deleteHelloAsync(Long helloId) {
-        helloRepository.deleteById(helloId);
+      LOGGER.info("_deleteHelloAsync ~ Start delete: {} at {}", helloId, System.currentTimeMillis());
+      try {
+        Thread.sleep(6000);
+      } catch (InterruptedException e) {
+        LOGGER.error("_deleteHelloAsync ~ Error: {}", e.getMessage());
+        Thread.currentThread().interrupt();
+      }
+      LOGGER.info("_deleteHelloAsync ~ End delete: {} at {}", helloId, System.currentTimeMillis());
+      helloRepository.deleteById(helloId);
     }
 }
